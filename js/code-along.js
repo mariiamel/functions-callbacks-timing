e1a4c536b0bef678f82d7e8b23ec38960653755c
mariiamel <mariiamelnikova96@gmail.com>
2021-01-27 12:05:34 -0800

    Team work with Sarah

--- js/code-along.js
+++ js/code-along.js
@@ -1,3 +1,24 @@
 /*  //  //  //  //  //  //  //  //
 this is a blank space to use for note taking during the code along!
 */  //  //  //  //  //  //  //  //
+
+function sayHello() {
+    console.log('Hello from a function');
+}
+// sayHello();
+
+
+let global = 'I am the global scope';
+
+function sharingIsCaring() {
+    console.log(globl)
+}
+sharingIsCaring();
+
+
+
+function addTwoNumbers(number1, number2) {
+    return number1 + number2;
+}
+let mySum = addTwoNumbers(10, 20)
+console.log(mySum)
--- js/functions-lab.js
+++ js/functions-lab.js
@@ -5,6 +5,12 @@ write a function that concatenates the string ' World' with the variable
 invoke the function
 log the variable's value after you invoke the function 
 */  //  //  //  //  //  //  //  //
+let sayHello = 'Hello';
+ function addWorld() {
+    console.log(sayHello + 'World')
+ }
+ 
+ addWorld()
 
 /*  //  //  //  //  //  //  //  //
 2. 
@@ -14,9 +20,29 @@ write a third function that prints result's value in the console
 invoke the three functions in order
 */  //  //  //  //  //  //  //  //
 
-let number1
-let number2
-let result
+let number1 = 0;
+let number2 = 0;
+let result = 0;
+
+function assignsValue(num1, num2){
+    number1 = num1;
+    number2 = num2;
+}
+assignsValue(1,3);
+console.log(number1, number2);
+
+function multipliesValue(number1, number2) {
+    let sum = number1 * number2;
+    return sum
+}
+const total = multipliesValue(3,5);
+console.log(total)
+
+function prints() {
+    console.log(number1, number2);
+    console.log(total);
+}
+prints()
 
 /*  //  //  //  //  //  //  //  //
 3. 
@@ -24,6 +50,13 @@ write a function that accepts a number as a parameter, squares it (multiplies it
 invoke the function and assign its return to a new variable
 print the variable
 */  //  //  //  //  //  //  //  //
+
+function squaresNumbers(number) {
+    return number * number;
+}
+
+let resulrSq = squaresNumbers(4);
+console.log(resulrSq);
 
 /*  //  //  //  //  //  //  //  //
 4. 
@@ -36,6 +69,20 @@ output: "54"
 example input: 67, 1
 output: "671"
 */  //  //  //  //  //  //  //  //
+let concNumber1 = 0;
+let concNumber2 = 0;
+let concatenate = '';
+
+function concatenateNum (concNum1, concNum2) {
+    concNumber1 = concNum1.toString();
+    concNumber2 = concNum2.toString();
+    concatenate = concNumber1 + concNumber2;
+    return concatenate
+}
+
+concatenateNum(3,2);
+console.log(concatenate);
+
 
 /*  //  //  //  //  //  //  //  //
 5. 
@@ -45,8 +92,15 @@ print the variable
 example input: 3, 15
 output: 15
 */  //  //  //  //  //  //  //  //
+let firstNum = 3;
+let secNum = 10;
+let bigger = 0;
 
+function compares (firstNum, secNum) {
+    if (firstNum > secNum) {
 
+    }
+}
 /*  //  //  //  //  //  //  //  //
 6. 
 create an object with several key: value pairs and assign it to a variable
